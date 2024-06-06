dadaf3d5b97a44b9008b826c3706b1bd7cfb455b
rlckyyy <reluckytryhrd@gmail.com>
2024-06-06 16:12:46 +0500

    init of project

---
+++ src/main/java/postingapp/finalsreproject/FinalSreProjectApplication.java
@@ -0,0 +1,13 @@
+package postingapp.finalsreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalSreProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/postingapp/finalsreproject/FinalSreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package postingapp.finalsreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
